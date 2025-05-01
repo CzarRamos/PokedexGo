d7019017ea6d3395c93410fae4f84c3431e1f7b8
CzarinaRamos <czar.ramos.co@gmail.com>
2025-05-01 14:22:42 -0700

    Added help, exit, and map commands.

--- main.go
+++ main.go
@@ -1,9 +1,91 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var commands = map[string]cliCommand{
+	"exit": {
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    commandExit,
+	},
+	"map": {
+		name:        "map",
+		description: "Lists all locations in the Pokemon world",
+		callback:    commandMap,
+	},
+}
+
+func init() {
+	commands["help"] = cliCommand{
+		name:        "help",
+		description: "Displays all commands",
+		callback:    commandHelp,
+	}
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		ok := scanner.Scan()
+		if !ok {
+			continue
+		}
+
+		cleanedInput := (cleanInput(scanner.Text()))[0]
+
+		output, ok := commands[cleanedInput]
+		if !ok {
+			fmt.Println("Unknown Command")
+			continue
+		}
+
+		err := output.callback()
+		if err != nil {
+			fmt.Println("Unknown Command")
+			continue
+		}
+	}
+
+}
+
+func cleanInput(text string) []string {
+	lowercasedText := strings.ToLower(text)
+	words := strings.Fields(lowercasedText)
+
+	return words
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Printf("# of CMDs: %d\n", len(commands))
+	for key, _ := range commands {
+		formattedOutput := fmt.Sprintf("%v: %v", commands[key].name, commands[key].description)
+		fmt.Println(formattedOutput)
+	}
+	return nil
+}
+
+func commandMap() error {
+	return nil
 }
---
+++ repl_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCleanInput(t *testing.T) {
+
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  Hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+
+		if len(c.expected) != len(actual) {
+			t.Errorf("The strings expected:%d and actual:%d are not the same length!", len(c.expected), len(actual))
+		}
+
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word != expectedWord {
+				t.Errorf("Words expected:%s and actual:%s does not match", expectedWord, word)
+			}
+		}
+	}
+}
